99fa33e1af70cb896ec72f2dfd4299f001f65115
Samuel Hausmann <samhausmann@Samuels-MacBook-Air.local>
2018-04-04 11:19:35 -0400

    writing results to json

--- build.sbt
+++ build.sbt
@@ -4,3 +4,6 @@ scalaVersion := "2.11.8"
 name := "olympics"
 
 libraryDependencies += "junit" % "junit" % "4.10" % Test
+libraryDependencies += "org.json4s"  %% "json4s-jackson" % "3.3.0"
+
+
--- src/main/scala/olympics.scala
+++ src/main/scala/olympics.scala
@@ -2,6 +2,11 @@ package olympics
 
 import java.io.InputStream
 import scala.io.Source
+import java.io.File
+import java.io.PrintWriter
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+import org.json4s.JsonDSL._
 
 object Olympics {
 
@@ -17,27 +22,18 @@ object Olympics {
   	// inital data projection into the form (Year, Country, Medal)
 	def parse(line: String): Medal = {
     	val l: List[String] = line.split(',').toList
-
-    	if (l.length == 10) {
-			val m: Medal = Medal(l(0), l(6), l(9))
-			m
-    	} else if (l.length == 9){
-    		val m: Medal = Medal(l(0), l(6), l(8))
-    		m
-    	} else {
-    		// maybe change this later
-    		Medal("x", "x", "x")
-    	}
+    	val m: Medal = Medal(l(0), l(6), l(l.length - 1))
+    	m
   	}
-
 
 	val medalList: List[Medal] = getDataStream.map(x => parse(x))
 
 	def main(args: Array[String]) {
-		
+
+		val writer = new PrintWriter(new File("src/main/resources/summer/outputjson.json"))
+	
 		// group country and medal pairings by year
 		val yearCountryMedalMap: Map[String, Map[String, List[String]]] = medalList
-		.filter(_.Country != "x")
 		.map(x => x.Year -> (x.Country, x.Medal))
 		.groupBy(_._1)
 		.map(x => (x._1, x._2.map(y => y._2)))
@@ -49,7 +45,11 @@ object Olympics {
 		// get all the medal counts
 		yearCountryMedalMap
 		.map{case (k, v) => (k, v.map(x => x._1 -> x._2.groupBy(identity).mapValues(_.size)))}
-		.foreach(x => println(x))
+
+		// convert to json and write to file to be used by frontend
+		val outputStr: String = compact(render(yearCountryMedalMap))
+		writer.write(outputStr)
+    	writer.close()
 	}
 
 }
